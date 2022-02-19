3a05d4a188fa3dc02d88ae853383c73426710ce2
Monel Cocou GAFFAN <Monel Cocou GAFFAN@MONELPC>
2022-02-19 22:49:28 +0100

    Insertion du formulaire d'insertion de categorie

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie', name: 'categorie')]
+    public function index(CategorieRepository $categorieRepository): Response
+    {
+        return $this->render('categorie/index.html.twig', [
+            'categories' => $categorieRepository->findAll(),
+        ]);
+    }
+
+
+    #[Route('/categorie/new', name: 'app_categorie_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager)
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $categorie->setLibelle(
+                $form->get('libelle')->getData()
+            );
+
+            $entityManager->persist($categorie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('categorie');
+        }
+
+
+        return $this->renderForm('categorie/new.html.twig',
+            [
+                'form' => $this->createForm(CategorieType::class, $categorie)
+            ]);
+    }
+}
