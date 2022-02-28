d77dd770f1ddc129dba6c82b94af7ce79873c589
Monel Cocou GAFFAN <Monel Cocou GAFFAN@MONELPC>
2022-02-28 08:25:51 +0100

    feat: Create et Real de l'entité User

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,6 @@
 import './styles/app.scss';
 import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/js/bootstrap.js'
+
+
+
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+//            ->add('roles', ChoiceType::class, [
+//                'choices' => [
+//                    'Admin' => 'ROLE_ADMIN',
+//                    'Gestionnaire' => 'ROLE_GESTIONNAIRE',
+//                    'Caissier' => 'ROLE_CAISSIER',
+//                ],
+//            ])
+
+//            ->add('roles', ChoiceType::class, [
+//                'choices' => [
+//                    'Admin' => 'ROLE_ADMIN',
+//                    'Gestionnaire' => 'ROLE_GESTIONNAIRE',
+//                    'Caissier' => 'ROLE_CAISSIER',
+//                ],
+//                'expanded'  => false, // liste déroulante
+//                'multiple'  => true, // choix multiple
+//            ])
+            ->add('roles', CollectionType::class, [
+                'entry_type'   => ChoiceType::class,
+                'entry_options'  => [
+                    'label' => false,
+                    'choices' => [
+                        'Admin' => 'ROLE_ADMIN',
+                        'Gestionnaire' => 'ROLE_GESTIONNAIRE',
+                        'Caissier' => 'ROLE_CAISSIER',
+                    ],
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
